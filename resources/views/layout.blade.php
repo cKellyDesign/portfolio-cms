2dd5b70f603c3fec7b864064155f2bd50dacf89d
Conor Kelly <ckelly@ckellydesign.net>
2015-10-06 21:29:34 -0700

    Init SignIn View

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <title>About</title>
+
+</head>
+<body>
+  <main>
+    @yield('content')
+  </main>
+  <footer>
+    @yield('footer')
+  </footer>
+</body>
+</html>
---
+++ resources/views/pages/signIn.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+
+<h1>Please Sign In</h1>
+
+@stop
